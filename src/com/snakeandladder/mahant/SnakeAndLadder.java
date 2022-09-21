088c3492122298dd0ea416e63be1102d29e8f69e
mahant95 <montyempire95@gmail.com>
2022-09-21 22:44:05 +0530

    Added Snake and Ladder Program

---
+++ src/com/snakeandladder/mahant/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.snakeandladder.mahant;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Welcome to Snake And Ladder Program"); 
+
+	}
+
+}
